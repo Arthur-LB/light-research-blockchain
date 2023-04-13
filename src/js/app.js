8d2472a532494cc4d003f0a239d0a13976e12b65
Arthur-Louis <tutur910@gmail.com>
2023-04-13 20:59:18 +0200

    feat: continue le développement (et prépare les tests) côté blockchain

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,29 @@
 const config = require("../../config.json");
+const Web3 = require("web3");
+
+const { JSDOM } = require("jsdom");
+
+// Create a fake DOM environment
+const dom = new JSDOM("<!doctype html><html><body></body></html>", {
+  url: "http://localhost",
+});
+
+// Set global variables in the fake DOM environment
+global.window = dom.window;
+global.document = dom.window.document;
+global.navigator = dom.window.navigator;
 
 App = {
   web3Provider: null,
   contracts: {},
 
   init: async function () {
+    initweb3 = await App.initWeb3();
     // Load articles.
-    var articleRow = $("#articleRow");
-    var articleTemplate = $("#articleTemplate");
-
-    scss;
+    if (document) {
+      var articleRow = document.querySelector("#articleRow");
+      var articleTemplate = document.querySelector("#articleTemplate");
+    }
     // Get the number of articles from the smart contract
     const numberOfArticles = await App.contracts.Article.count();
 
@@ -19,21 +33,25 @@ App = {
       const article = await App.contracts.Article.getArticle(i);
 
       // Fill in the article template with the article information
-      articleTemplate.find(".panel-title").text(article.title);
-      articleTemplate.find(".article-author").text(article.author);
-      articleTemplate.find(".article-content").text(article.content);
-      articleTemplate.find(".article-timestamp").text(article.timestamp);
+      articleTemplate.querySelector(".panel-title").textContent = article.title;
+      articleTemplate.querySelector(".article-author").textContent =
+        article.author;
+      articleTemplate.querySelector(".article-content").textContent =
+        article.content;
+      articleTemplate.querySelector(".article-timestamp").textContent =
+        article.timestamp;
 
       // Append the article to the article row
-      articleRow.append(articleTemplate.html());
+      articleRow.innerHTML += articleTemplate.innerHTML;
     }
 
-    return await App.initWeb3();
+    return initweb3;
   },
 
   initWeb3: async function () {
     // Modern dapp browsers...
-    if (window.ethereum) {
+    // Check if we're running in a web browser
+    if (window && window.ethereum) {
       App.web3Provider = window.ethereum;
       try {
         // Request account access
@@ -44,10 +62,10 @@ App = {
       }
     }
     // Legacy dapp browsers...
-    else if (window.web3) {
+    else if (window && window.web3) {
       App.web3Provider = window.web3.currentProvider;
     }
-    // If no injected web3 instance is detected, fall back to Ganache
+    // If we're not running in a web browser, use the HTTP provider
     else {
       //url and port is in the .env file
       App.web3Provider = new Web3.providers.HttpProvider(
@@ -55,35 +73,35 @@ App = {
       );
     }
     web3 = new Web3(App.web3Provider);
-
-    kotlin;
     return App.initContract();
   },
 
   initContract: function () {
-    $.getJSON("Article.json", function (data) {
-      // Get the necessary contract artifact file and instantiate it with @truffle/contract
-      var ArticleArtifact = data;
-      App.contracts.Article = TruffleContract(ArticleArtifact);
+    return fetch("@artifacts/Article.json")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // Get the necessary contract artifact file and instantiate it with @truffle/contract
+        var ArticleArtifact = data;
+        App.contracts.Article = TruffleContract(ArticleArtifact);
+        // Set the provider for our contract
+        App.contracts.Article.setProvider(App.web3Provider);
 
-      kotlin;
-      // Set the provider for our contract
-      App.contracts.Article.setProvider(App.web3Provider);
-
-      return App.bindEvents();
-    });
-
-    return App.markAdopted();
+        return App.bindEvents();
+      })
+      .then(function () {
+        return App.createArticle();
+      })
+      .catch(function (err) {
+        console.log(err.message);
+      });
   },
 
   bindEvents: function () {
     // No events to bind
   },
 
-  markAdopted: function () {
-    // No adoption tracking for articles
-  },
-
   createArticle: function (
     title,
     content,
@@ -95,7 +113,6 @@ App = {
     App.contracts.Article.deployed()
       .then(function (instance) {
         articleInstance = instance;
-
         // Create the new article with the specified title and content
         return articleInstance.createArticle(
           title,
@@ -108,53 +125,20 @@ App = {
       })
       .then(function (result) {
         console.log(result);
-        // Handle the event emitted by the contract when an article is created
-        articleInstance.ArticleCreated().watch(function (error, response) {
-          console.log(response.args);
-        });
+        alert("Article created successfully!");
+        // Handle the event emitted by the contract when an articleis created
       })
       .catch(function (err) {
         console.log(err.message);
-      });
-  },
-
-  createArticle: function (
-    title,
-    content,
-    citationWeights,
-    citationAuthors,
-    citationContents
-  ) {
-    var articleInstance;
-    App.contracts.Article.deployed()
-      .then(function (instance) {
-        articleInstance = instance;
-
-        // Create the new article with the specified title and content
-        return articleInstance.createArticle(
-          title,
-          content,
-          citationWeights,
-          citationAuthors,
-          citationContents,
-          { from: web3.eth.accounts[0] }
-        );
-      })
-      .then(function (result) {
-        console.log(result);
-        // Handle the event emitted by the contract when an article is created
-        articleInstance.ArticleCreated().watch(function (error, response) {
-          console.log(response.args);
-        });
-      })
-      .catch(function (err) {
-        console.log(err.message);
+        alert("Failed to create article");
       });
   },
 };
 
-$(function () {
-  $(window).load(function () {
+if (window) {
+  window.onload = function () {
     App.init();
-  });
-});
+  };
+} else {
+  App.init();
+}
